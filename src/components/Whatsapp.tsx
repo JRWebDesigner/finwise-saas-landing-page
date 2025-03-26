6ce0c52f12403d67ae70e9cf9bd182cacc22970b
JRwebdesing <jowebdesigc@gmail.com>
2025-03-26 07:17:48 -0400

    sanity

--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -2,7 +2,7 @@ export default function Whatsapp(){
     return(
         <div className="fixed bottom-5 right-5 z-40 p-3 rounded-full bg-green-600">
             <a href="https://wa.me/59177793456" target="_blank" rel="noreferrer">
-                <img src="/images/whatsapp.svg" alt="whatsapp" className="w-16 h-16" />
+                <img src="/images/whatsapp.svg" alt="whatsapp" className="w-16 h-16" /> 
             </a>
         </div>
     )
