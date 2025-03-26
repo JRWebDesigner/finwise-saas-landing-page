0b71ad973781276735b8372fe7897beda0f2c534
JRwebdesing <jowebdesigc@gmail.com>
2025-03-26 10:26:20 -0400

    getcategorys

--- src/components/Productos/GetCategorys.tsx
+++ src/components/Productos/GetCategorys.tsx
@@ -1,25 +1,34 @@
 import Link from 'next/link';
-import {allCategoriesQuery} from '@/sanity/lib/queries';
-import { SanityDocument } from 'next-sanity';
+import { allCategoriesQuery } from '@/sanity/lib/queries';
 import { sanityFetch } from '@/sanity/lib/fetch';
 
+// Define una interfaz específica para las categorías
+interface Category {
+  _id: string;
+  Nombre: string;
+  Enlace: string;
+}
+
 export default async function GetCategorys() {
-  const categorys = await sanityFetch<SanityDocument[]>({ query: allCategoriesQuery });
-    console.log(categorys)
-    return (
-        <ul>
-            <li>
-                <Link href='/productos'>
-                    Todos
-                </Link>
-            </li>
-            {categorys.map((category: { _id: string; Nombre: string; Enlace: string }) => (
-                <li key={category._id}>
-                    <Link href={`/productos/${category.Enlace}`}>
-                        {category.Nombre}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    )
+  // Especifica que sanityFetch devuelve un array de Category
+  const categories = await sanityFetch<Category[]>({ query: allCategoriesQuery });
+
+  console.log(categories);
+
+  return (
+    <ul>
+      <li>
+        <Link href="/productos">
+          Todos
+        </Link>
+      </li>
+      {categories.map((category) => (
+        <li key={category._id}>
+          <Link href={`/productos/${category.Enlace}`}>
+            {category.Nombre}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
