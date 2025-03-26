621710761dbea795c91a444f7097fba06b530007
JRwebdesing <jowebdesigc@gmail.com>
2025-03-26 10:53:02 -0400

    getcategorys

--- src/components/Productos/GetCategorys.tsx
+++ src/components/Productos/GetCategorys.tsx
@@ -1,24 +1,32 @@
 import Link from 'next/link';
-import {client} from '@/sanity/lib/client';
-import {allCategoriesQuery} from '@/sanity/lib/queries';
+import { client } from '@/sanity/lib/client';
+import { allCategoriesQuery } from '@/sanity/lib/queries';
+
+// Define una interfaz específica para las categorías
+interface Category {
+  _id: string;
+  Nombre: string;
+  Enlace: string;
+}
 
 export default async function GetCategorys() {
-    const categorys = await client.fetch(allCategoriesQuery);
+  // Usa la consulta definida en queries.ts
+  const categories: Category[] = await client.fetch(allCategoriesQuery);
 
-    return (
-        <ul>
-            <li>
-                <Link href='/productos'>
-                    Todos
-                </Link>
-            </li>
-            {categorys.map((category: { _id: string; Nombre: string; Enlace:string; }) => (
-                <li key={category._id}>
-                    <Link href={`/productos/${category.Enlace}`}>
-                        {category.Nombre}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    )
+  return (
+    <ul>
+      <li>
+        <Link href="/productos">
+          Todos
+        </Link>
+      </li>
+      {categories.map((category) => (
+        <li key={category._id}>
+          <Link href={`/productos/${category.Enlace}`}>
+            {category.Nombre}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
