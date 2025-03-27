e6ac6ef0861ad81e6a00fed58811d81e50098352
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-03-27 17:40:03 -0400

    Update queries.ts

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,8 +1,8 @@
 import {groq} from 'next-sanity'
 
-export const allProductsQuery = groq`*[_type == "producto" | order(_createdAt desc)]{
-  _id,_createdAt, nombre, "mainImage":image.asset->url, categoria, descripcion
+export const allProductsQuery = groq`*[_type == "producto"] | order(_createdAt desc) {
+  _id, _createdAt, nombre, "mainImage": image.asset->url, categoria, descripcion
 }`
-export const allCategoriesQuery = groq`*[_type == "category"]{
-  _id, nombre, "enlace": enlace.current
+export const allCategoriesQuery = groq`*[_type == "category"]| order(_createdAt desc){
+  _id,_createdAt, nombre, "enlace": enlace.current
 }`
