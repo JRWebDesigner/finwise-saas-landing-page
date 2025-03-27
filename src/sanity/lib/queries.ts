60ed5c7faeff2abdccefeacd12697df4fdf9ab6a
JRwebdesing <jowebdesigc@gmail.com>
2025-03-27 15:45:33 -0400

    intento 7

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -3,6 +3,6 @@ import {groq} from 'next-sanity'
 export const allProductsQuery = groq`*[_type == "producto" | order(_createdAt desc)]{
   _id,_createdAt, nombre, "mainImage":image.asset->url, categoria, descripcion
 }`
-export const allCategoriesQuery = groq`*[_type == "category" | order(_createdAt desc)]{
+export const allCategoriesQuery = groq`*[_type == "category"{
   _id, nombre, "enlace": enlace.current
 }`
