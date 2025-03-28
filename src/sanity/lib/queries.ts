219cdd866630ae83ab2b124e14ae7de2c56999e6
JRwebdesing <jowebdesigc@gmail.com>
2025-03-28 01:16:00 -0400

    productos

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import {groq} from 'next-sanity'
 
 export const allProductsQuery = groq`*[_type == "producto"] | order(_createdAt desc) {
-  _id, _createdAt, nombre, "mainImage": image.asset->url, categoria, descripcion
+  _id, _createdAt, nombre, "mainImage": mainImage.asset->url, categoria, descripcion
 }`
 export const allCategoriesQuery = groq`*[_type == "category"]| order(_createdAt desc){
   _id,_createdAt, nombre, "enlace": enlace.current
