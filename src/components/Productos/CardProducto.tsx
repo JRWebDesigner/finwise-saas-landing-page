1056882c92372f2adf15127a5d7328dd838918ae
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-03-29 14:28:16 -0400

    Update CardProducto.tsx

--- src/components/Productos/CardProducto.tsx
+++ src/components/Productos/CardProducto.tsx
@@ -76,7 +76,7 @@ export default function CardProducto({ nombre, mainImage, marca, descripcion }: 
                             <div className="flex-center w-full my-2">
                                 <span className="text-sm text-gray-500 text-primary">{marca}</span>
                             </div>
-                            <div className='overflow-y-auto w-full max-h-[200px] md:max-h-[400px] p-4 prose prose-xl'>
+                            <div className='overflow-y-auto w-full max-h-[200px] md:max-h-[400px] prose prose-xl'>
                                 <h3>Descripción:</h3>
 				<PortableText value={descripcion} />
                             </div>
