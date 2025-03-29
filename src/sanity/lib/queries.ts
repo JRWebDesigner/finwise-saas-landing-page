614ce1b59eda6ee734fa501775fdc483f6c40f1f
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-03-29 14:35:00 -0400

    Update queries.ts

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -2,7 +2,7 @@ import {groq} from 'next-sanity'
 import { client } from './client';
 
 export const allProductsQuery = groq`*[_type == "producto"] | order(_createdAt desc) {
-  _id, _createdAt, nombre, "mainImage": mainImage.asset->url, "categorias": categoria[0]->enlace.current, descripcion
+  _id, _createdAt, nombre, "mainImage": mainImage.asset->url, "categorias": categoria[0]->enlace.current, marca, descripcion
 }`
 export const allCategoriesQuery = groq`*[_type == "category"]| order(_createdAt desc){
   _id,_createdAt, nombre, "enlace": enlace.current
@@ -10,7 +10,7 @@ export const allCategoriesQuery = groq`*[_type == "category"]| order(_createdAt 
 export async function getProductCategory(categorySlug: string) {
   return client.fetch(
     groq`*[_type == "producto" && categoria[0]->enlace.current == $categorySlug] | order(_createdAt desc) {
-       _id, _createdAt, nombre, "mainImage": mainImage.asset->url, "categorias": categoria[0]->enlace.current, descripcion
+       _id, _createdAt, nombre, "mainImage": mainImage.asset->url, "categorias": categoria[0]->enlace.current, marca, descripcion
     }`,
     { categorySlug }
   );
