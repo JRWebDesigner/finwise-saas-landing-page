8f6a6174e5fb54db1acafe9dbe36986a3dcee5b0
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-03 16:03:56 -0400

    Jos el capo

--- src/components/AboutPage/ContainAboutPage.tsx
+++ src/components/AboutPage/ContainAboutPage.tsx
@@ -26,9 +26,9 @@ export default function ContainAboutPage(){
               whileInView="onscreen"
               viewport={{ once: true }}
 						>
-						<h2 className='font-bold text-2xl md:text-3xl'>Nuestra Historia</h2>
+						<h2 className='font-bold text-2xl md:text-3xl'>Éxito Competitivo</h2>
 						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor laboriosam reprehenderit magnam quis expedita accusamus ea provident architecto, perferendis suscipit molestias excepturi esse totam labore. Aliquid voluptate corporis officiis voluptatem consequuntur necessitatibus porro dolorem velit recusandae alias. Facilis voluptatem ex soluta blanditiis inventore ad culpa nesciunt, exercitationem molestiae, voluptatum hic tempora libero sit explicabo quibusdam. Consequatur, quae? Similique in accusamus totam, non necessitatibus sit eveniet corrupti officiis est. Pariatur et unde excepturi quod magnam velit quaerat numquam, eum assumenda ipsa error repellendus? Voluptatibus, enim odio. Consequatur blanditiis tenetur dolorum error corrupti! Nostrum soluta reprehenderit dolore cumque facilis magni tempora quam magnam nesciunt consectetur, labore laborum eveniet adipisci totam exercitationem! Temporibus officiis fugit illo, dolor inventore quia enim! Veritatis ullam sunt repudiandae voluptatum eius quasi hic, nobis velit quaerat tenetur obcaecati eos ipsa maxime nam commodi. Quo quia unde, sed nobis iure aspernatur, repellendus aut illum repellat itaque numquam perspiciatis doloremque obcaecati ullam omnis reiciendis ipsum laudantium vero aliquid, tenetur ea.
+							El éxito competitivo de N-INNOVATIONS BIOSUPPLY BOL. LTDA. no sólo se debe a la integración de la organización como un todo, sino a la especialización que tiene cada área para la realización adecuada de sus actividades y la percepción ante las necesidades futuras de nuestros clientes. Nuestro capital humano a su vez, cuenta con el conocimiento técnico necesario para ofrecer la solución que los clientes requieren de forma eficiente y adecuada para la toma de decisiones.
 						</p>
 						</motion.div>
 					</section>
@@ -43,14 +43,16 @@ export default function ContainAboutPage(){
 						<div>
 							<h2 className='font-bold text-xl md:text-2xl'>Mision</h2>
 							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, doloribus sint quos quas labore corrupti reiciendis delectus ipsum sapiente suscipit architecto, tempora excepturi dolore ea voluptates vero voluptas iusto? Tempore, modi at! Dolore odit, odio rerum debitis nam, ut tenetur aliquid iure blanditiis illum, vitae quis! Vero est consectetur id velit ut. Provident sapiente impedit a dolore ipsum tempora alias?
-							</p>
+								Somos una empresa privada importadora y distribuidora de equipos, consumibles y reactivos químicos de laboratorio con cobertura a lo largo de todo el país, para satisfacer las necesidades en el sector industrial, analítico y de investigación, dando una atención personalizada a nuestros clientes en las áreas de venta, postventa y asesoría técnica, gracias a que contamos un equipo de trabajo especializado y altamente comprometido.
+								
 						</div>
 						<div>
 							<h2 className='font-bold text-xl md:text-2xl'>Vision</h2>
 							<p>
-								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut et doloremque nesciunt, quidem pariatur odio temporibus obcaecati tempore veritatis culpa velit est quaerat sint excepturi voluptas quis ad quae aspernatur a debitis? Odit tempore recusandae similique obcaecati reprehenderit illum fugiat ipsam enim, odio rerum blanditiis. Porro, ab voluptate pariatur dolorum architecto id ullam iure ad officia reprehenderit quo voluptatibus accusantium!
+								Ser una empresa líder en Bolivia de distribución de productos químicos, que aporten al quehacer diario del país y al desarrollo científico-tecnológico otorgando un servicio de excelencia a nuestros clientes.
+
 							</p>
+								 
 						</div>
 					</motion.div>
         </>
